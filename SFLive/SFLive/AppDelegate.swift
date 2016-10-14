aa0a47d437f94ba01b651a90c5b4c0b61a4beef4
maodenden <MaoDenden@qq.com>
2016-10-14 13:02:13 +0800

    ---------

--- SFLive/SFLive/AppDelegate.swift
+++ SFLive/SFLive/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        if let w = window {
+            w.backgroundColor = UIColor.white
+            let main = MainViewController()
+            w.rootViewController = main
+            w.makeKeyAndVisible()
+        }
         return true
     }
 
---
+++ SFLive/SFLive/ViewController/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  SFLive
+//
+//  Created by MaoDenden on 2016/10/14.
+//  Copyright © 2016年 zhangxianjun. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
